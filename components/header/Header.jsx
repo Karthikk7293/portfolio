0d2cdcf64ffb90a22f729a9cfdf5fa8ea0fe9b8f
Karthikk7293 <karthikk7293@gmail.com>
2024-10-19 09:15:02 +0530

    🐛 FIX: commented the image tag in header

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,19 +1,17 @@
-import Image from "next/image";
 import Link from "next/link";
-import star from "@/public/star.svg"
 
 function Header() {
   return (
     <div className="  flex justify-between pt-16 pb-4 ">
       <div className=" w-1/2 flex justify-center items-center ">
         <div className="relative">
-          <Image
+          {/* <Image
             src={star}
             alt="star "
             width={42}
             height={56}
             className="absolute -left-11 -top-4"
-          />
+          /> */}
           <p className={`font-baggers text-[#E7E7E7] text-[36px] `}>
             PORTFOLIO
           </p>
